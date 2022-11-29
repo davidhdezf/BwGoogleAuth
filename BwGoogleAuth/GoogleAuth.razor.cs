37d28ea35a518510d491a4630a1550104b573dea
David Hernandez Flores <david.hernandez@aspentech.com>
2022-11-28 20:43:55 -0600

    Add project files.

---
+++ BwGoogleAuth/GoogleAuth.razor.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BwGoogleAuth
+{
+    public partial class GoogleAuth
+    {
+
+     
+        [Parameter]
+        public String ClientId { get; set; }
+
+
+        [Parameter]
+        public bool Hide { get; set; } = false;
+
+        [Parameter]
+        public Credential UserCredential { get; set; }
+
+
+        private DotNetObjectReference<GoogleAuth> objRef;
+
+
+     
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+
+
+            if (firstRender)
+            {
+                objRef = DotNetObjectReference.Create(this);
+
+
+                await JsRuntime.InvokeVoidAsync("bwjs.initGoogle", new object[] { objRef, ClientId });
+            }
+
+
+
+        }
+
+        [JSInvokable]
+        public async Task SaveCredentials(string mycredential)
+        {
+
+            var handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken decodedValue = handler.ReadJwtToken(mycredential);
+
+            UserCredential.Email = (string)decodedValue.Payload["email"];
+
+            UserCredential.UserId = (string)decodedValue.Payload["sub"];
+                  
+            UserCredential.Name = (string)decodedValue.Payload["given_name"];
+
+
+
+            UserCredential.IsLogged = true;
+
+           await UserCredentialChanged.InvokeAsync(UserCredential);
+
+            StateHasChanged();
+        }
+
+        //Automatically binded by @bind
+        //https://www.syncfusion.com/faq/blazor/components/how-do-you-pass-values-from-child-to-parent-using-eventcallback-in-blazor
+        [Parameter]
+        public EventCallback<Credential> UserCredentialChanged { get; set; }
+
+        //private Task OnUserCredetialChanged(ChangeEventArgs e)
+        //{
+        
+        //    return UserCredentialChanged.InvokeAsync(UserCredential);
+        //}
+
+
+        public void Dispose()
+        {
+            objRef?.Dispose();
+        }
+
+
+
+    }
+}
---
+++ BwGoogleAuth/wwwroot/JsInterop.js
@@ -0,0 +1,39 @@
+// This is a JavaScript module that is loaded on demand. It can export any number of
+// functions, and may import other JavaScript modules if required.
+
+var bwjs = {}
+
+
+
+
+
+bwjs.initGoogle = function (dotNetObjectRef,clientid) {
+
+    handleCredentialResponse = function (response) {
+
+        console.log("Encoded JWT ID token: " + response.credential);
+
+        dotNetObjectRef.invokeMethodAsync('SaveCredentials', response.credential);
+    }
+
+
+
+
+    google.accounts.id.initialize({
+        client_id: clientid,
+        callback: handleCredentialResponse
+    });
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+    );
+    //  google.accounts.id.prompt(); // also display the One Tap dialog
+}
+
+
+
+
+
+
+
+//secret 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
