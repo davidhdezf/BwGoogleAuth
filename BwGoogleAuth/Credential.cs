586a7b218135266a735bb26f0efe49116e960ca0
David Hernandez Flores <david.hernandez@aspentech.com>
2022-11-28 20:43:55 -0600

    Add project files.

---
+++ BwGoogleAuth/Credential.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BwGoogleAuth
+{
+    public class Credential
+    {
+        public String UserId { get; set; } = "";
+
+
+        public String Email { get; set; } = "";
+
+
+
+        public String Name { get; set; } = "";
+
+
+
+        public bool IsLogged { get; set; } = false;
+
+    }
+}
